8ae1201f87a3710559af3fea70bfe88bec9bd8de
Adrian <>
2024-03-07 18:43:47 -0500

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,11 @@ const Navbar = () => {
     return (
         <nav class="bg-blue-900 float-left w-full h-24">
             <ul class="align-middle">
-                <li class="text-white float-left px-12 py-5 hover:bg-blue-950 font-bold font-sans"><Link to="/"><img class="object-scale-down h-14 w-20" src={metubelogo} alt={"site logo"}/></Link></li>
-                <li class="text-white float-left px-8 py-9 hover:bg-blue-950 font-bold font-sans"><Link to="/showslist">Movies & TV Shows</Link></li>
-                <li class="text-white float-right px-8 py-9 hover:bg-blue-950 font-bold font-sans"><Link to="/login">Log in</Link></li>
-                <li class="text-white float-right px-8 py-9 hover:bg-blue-950 font-bold font-sans"><Link to="/signup">Sign Up</Link></li>
+                <li><Link to="/"><button class="text-white hover:bg-blue-950 float-left font-bold font-sans px-12 py-5"><img class="object-scale-down h-14 w-20" src={metubelogo} alt={"site logo"}/></button></Link></li>
+                <li><Link to="/showslist"><button class="text-white hover:bg-blue-950 float-left font-bold font-sans px-8 py-9">Movies & TV Shows</button></Link></li>
+                <li><Link to="/login"><button class="text-white hover:bg-blue-950 float-right font-bold font-sans px-8 py-9">Log in</button></Link></li>
+                <li><Link to="/signup"><button class="text-white hover:bg-blue-950 float-right font-bold font-sans px-8 py-9">Sign Up</button></Link></li>
             </ul>
-
-         
         </nav>  
     )
 }
